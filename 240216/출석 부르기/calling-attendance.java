26b650f5199c3b34e0db88cf9e3afc4aba56ef66
Kang Juhyeong <kakao_panda@naver.com>
2024-02-16 15:19:09 +0900

    Title: 출석 부르기, Time: 164ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240216/출석 부르기/calling-attendance.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String name = "";
+
+        if(n==1){
+            name = "John";
+        }
+        else if(n==2){
+            name = "Tom";
+        }
+        else if(n==3){
+            name = "Paul";
+        }
+        else{
+            name = "Vacancy";
+        }
+        System.out.println(name);
+    }
+}
