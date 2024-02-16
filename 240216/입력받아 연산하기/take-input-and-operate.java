212efcf9fa2a8a940f904f29976b38786fccc572
Kang Juhyeong <kakao_panda@naver.com>
2024-02-16 13:33:21 +0900

    Title: 입력받아 연산하기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240216/입력받아 연산하기/take-input-and-operate.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt() + 87;
+        int b = sc.nextInt() % 10;
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
