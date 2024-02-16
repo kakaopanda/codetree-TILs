7029486f5461d1fd7e6c36aa87693e71dc174204
Kang Juhyeong <kakao_panda@naver.com>
2024-02-16 13:11:05 +0900

    Title: 날짜 변경하여 출력, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240216/날짜 변경하여 출력/print-date-with-different-format.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringTokenizer st = new StringTokenizer(sc.next(), ".");
+        int year = Integer.parseInt(st.nextToken());
+        int month = Integer.parseInt(st.nextToken());
+        int day = Integer.parseInt(st.nextToken());
+
+        System.out.println(month + "-" + day + "-" + year);
+    }
+}
