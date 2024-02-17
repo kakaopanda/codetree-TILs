1fa92261047e3b1f8f4690a0c7a1e02c51210096
Kang Juhyeong <kakao_panda@naver.com>
2024-02-17 18:24:24 +0900

    Title: 일 수 구하기, Time: 140ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240217/일 수 구하기/number-of-days-in-month.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(2020, n-1, 1);
+        System.out.println(cal.getActualMaximum(Calendar.DATE));        
+    }
+}
