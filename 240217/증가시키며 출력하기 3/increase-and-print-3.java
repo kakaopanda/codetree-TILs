e08d5edef144a0b7b471c070611200b22993d82a
Kang Juhyeong <kakao_panda@naver.com>
2024-02-17 20:02:03 +0900

    Title: 증가시키며 출력하기 3, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240217/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        while(true){
+            if(i>26){
+                break;
+            }
+            else{
+                System.out.print(i + " ");
+                i += 2;
+            }
+        }
+    }
+}
