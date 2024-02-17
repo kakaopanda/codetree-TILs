d1f0b77523d444c2886bf3e81a95c6551220b4b5
Kang Juhyeong <kakao_panda@naver.com>
2024-02-17 20:27:24 +0900

    Title: 숫자의 증감, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240217/숫자의 증감/increasing-and-decreasing-numbers.java
+++ 240217/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -12,7 +12,7 @@ public class Main {
             }
         }
         else if(s.equals("D")){
-            for(int i=n; n>=1; i--){
+            for(int i=n; i>=1; i--){
                 System.out.print(i + " ");
             }
         }
