9c357cc9d3922daa8e7373a2931cdd56c90c5699
Kang Juhyeong <kakao_panda@naver.com>
2024-02-17 20:27:08 +0900

    Title: 숫자의 증감, Time: 3061ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

---
+++ 240217/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int n = sc.nextInt();
+
+        if(s.equals("A")){
+            for(int i=1; i<=n; i++){
+                System.out.print(i + " ");
+            }
+        }
+        else if(s.equals("D")){
+            for(int i=n; n>=1; i--){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
