480895410c756729fdc6be72e36b4777e337e193
Kang Juhyeong <kakao_panda@naver.com>
2024-02-17 20:07:27 +0900

    Title: 감소시키며 출력하기 2, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240217/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 26;
+        while(true){
+            if(i<10){
+                break;
+            }
+            else{
+                System.out.print(i + " ");
+                i -= 1;
+            }
+        }
+    }
+}
