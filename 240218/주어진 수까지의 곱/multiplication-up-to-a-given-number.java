0c26ac110cd40e5f48dabc3ac0945fd917e82a64
Kang Juhyeong <kakao_panda@naver.com>
2024-02-18 19:10:31 +0900

    Title: 주어진 수까지의 곱, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240218/주어진 수까지의 곱/multiplication-up-to-a-given-number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int mul = 1;
+
+        for(int i=a; i<=b; i++){
+            mul *= i;
+        }
+        System.out.println(mul);
+    }
+}
