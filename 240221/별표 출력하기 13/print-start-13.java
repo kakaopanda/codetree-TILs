8283a7c2a218d4ae15514da34a91ecbbaafa2c5e
Kang Juhyeong <kakao_panda@naver.com>
2024-02-21 14:36:17 +0900

    Title: 별표 출력하기 13, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240221/별표 출력하기 13/print-start-13.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=1; i<=2*n; i++){
+            if(i%2 == 1){
+                for(int j=n-(i/2); j>0; j--){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+            else{
+                for(int j=i/2; j>0; j--){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
