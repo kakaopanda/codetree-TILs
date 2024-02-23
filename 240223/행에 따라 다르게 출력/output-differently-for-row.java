54dc9b60cf4d058f1c238ef22d60109930bfd295
Kang Juhyeong <kakao_panda@naver.com>
2024-02-23 13:46:35 +0900

    Title: 행에 따라 다르게 출력, Time: 252ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240223/행에 따라 다르게 출력/output-differently-for-row.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int cnt = 0;
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<n; j++){
+                if(i%2 == 1){
+                    cnt += 1;
+                }
+                else{
+                    cnt += 2;
+                }
+                System.out.print(cnt + " ");
+            }
+            System.out.println();
+        }
+    }
+}
