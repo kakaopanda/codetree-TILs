f1b3fb3a750142fed3c95cfe52ccc6618851bf15
Kang Juhyeong <kakao_panda@naver.com>
2024-02-23 13:14:52 +0900

    Title: 좌우로 반복하며 출력, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240223/좌우로 반복하며 출력/print-in-left-right-reverse.java
+++ 240223/좌우로 반복하며 출력/print-in-left-right-reverse.java
@@ -5,14 +5,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int cnt = 4;
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 if(i%2==0){
                     System.out.print(j+1);
                 }
                 else{
-                    System.out.print(cnt-j);
+                    System.out.print(n-j);
                 }
             }
             System.out.println();
