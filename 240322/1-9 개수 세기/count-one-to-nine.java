99f598ed20303339a8db3a49a4cc06bb992743fb
Kang Juhyeong <kakao_panda@naver.com>
2024-03-22 01:29:43 +0900

    Title: 1-9 개수 세기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240322/1-9 개수 세기/count-one-to-nine.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            int k = sc.nextInt();
+            arr[k]++;
+        }
+
+        for(int i=1; i<10; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
