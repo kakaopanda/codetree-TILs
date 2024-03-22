4cdc0665eb44ca5bfb77fdd8c2267337fa613ae4
Kang Juhyeong <kakao_panda@naver.com>
2024-03-22 16:15:10 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 232ms, Memory: 13MB, Status: Passed - CodeTree

--- 240322/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
+++ 240322/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -10,7 +10,7 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
-        int max = Integer.MIN_VALUE;
+        int max = 0;
         for(int i=0; i<n; i++){
             int start = arr[i];
 
