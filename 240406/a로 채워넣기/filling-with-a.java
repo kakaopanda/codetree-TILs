15397f629f98e7d88294e1c9d82863ec7279c99f
Kang Juhyeong <kakao_panda@naver.com>
2024-04-06 15:38:10 +0900

    Title: a로 채워넣기, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240406/a로 채워넣기/filling-with-a.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        char[] arr = s.toCharArray();
+
+        arr[1] = 'a';
+        arr[arr.length-2] = 'a';
+
+        System.out.println(new String(arr));
+    }
+}
