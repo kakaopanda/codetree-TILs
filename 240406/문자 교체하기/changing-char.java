4d760b7b1c5a6980bedee8d604732c905d0b3c1d
Kang Juhyeong <kakao_panda@naver.com>
2024-04-06 15:42:18 +0900

    Title: 문자 교체하기, Time: 113ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240406/문자 교체하기/changing-char.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+
+        s2 = s2.replaceFirst(s2.substring(0,2), s1.substring(0,2));
+
+        System.out.println(s2);
+    }
+}
