033f289e38ecec45d129cc3bf0fe1c540a11429e
Kang Juhyeong <kakao_panda@naver.com>
2024-04-06 15:00:21 +0900

    Title: 문자열에서 문자 출력, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240406/문자열에서 문자 출력/print-chars-from-word.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        char[] arr = s.toCharArray();
+
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
