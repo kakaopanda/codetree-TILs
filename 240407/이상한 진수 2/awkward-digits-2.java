b3bcd555764c7639360108fa39ad81914918b60e
Kang Juhyeong <kakao_panda@naver.com>
2024-04-07 15:26:19 +0900

    Title: 이상한 진수 2, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240407/이상한 진수 2/awkward-digits-2.java
+++ 240407/이상한 진수 2/awkward-digits-2.java
@@ -6,11 +6,18 @@ public class Main {
         String a = sc.next();
         char[] arr = a.toCharArray();
 
+        boolean check = true;
         for(int i=1; i<arr.length; i++){
             if(arr[i] == '0'){
                 arr[i] = '1';
+                check = false;
                 break;
             }
+        }
+
+        // 모든 수가 1로 이루어져 한번도 변환되지 않은 경우
+        if(check){
+            arr[arr.length-1] = '0';
         }
 
         System.out.println(Integer.parseInt(new String(arr),2));
