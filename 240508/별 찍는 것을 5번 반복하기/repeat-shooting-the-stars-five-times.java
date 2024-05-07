1a6647473e15578466c0b9a6f0771ad319ddb497
Kang Juhyeong <kakao_panda@naver.com>
2024-05-08 01:35:52 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240508/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        for(int i=0; i<5; i++){
+            print();
+        }
+    }
+
+    public static void print(){
+        System.out.println("**********");
+    }
+}
