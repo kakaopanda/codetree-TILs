36713c095584f19fca48badc4abe41d83925eb88
Kang Juhyeong <kakao_panda@naver.com>
2024-05-09 13:10:20 +0900

    Title: 사칙연산 함수, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 240509/사칙연산 함수/quadratic-operations-function.java
+++ 240509/사칙연산 함수/quadratic-operations-function.java
@@ -20,6 +20,8 @@ public class Main {
             case "*":
                 multiple(a, c);
                 break;
+            default:
+                System.out.println("False");
         }
     }
 
