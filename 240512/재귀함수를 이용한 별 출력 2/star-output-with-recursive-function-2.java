c8dbc39f6d092b2307c4ee91569fd31cb003dcde
Kang Juhyeong <kakao_panda@naver.com>
2024-05-12 16:52:56 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 331ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240512/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        printStar(n);
+    }
+
+    public static void printStar(int n){
+        if(n == 0){
+            return;
+        }
+
+        for(int i=n; i>0; i--){
+            System.out.print("* ");
+        }
+        System.out.println();
+        printStar(n-1);
+        for(int i=n; i>0; i--){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+}
