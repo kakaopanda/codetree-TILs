7a05737efbc5b32cd0694e5df78fdda831cbeb1e
Kang Juhyeong <kakao_panda@naver.com>
2024-05-18 23:56:00 +0900

    Title: 줄 세우기 2, Time: 271ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240518/줄 세우기 2/line-up-students-2.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        ArrayList<Student> list = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            list.add(new Student(i+1, sc.nextInt(), sc.nextInt()));
+        }
+        Collections.sort(list);
+
+        for(int i=0; i<list.size(); i++){
+            System.out.println(list.get(i));
+        }
+    }
+}
+
+class Student implements Comparable<Student>{
+    int index;
+    int height;
+    int weight;
+
+    public Student(int i, int h, int w){
+        index = i;
+        height = h;
+        weight = w;
+    }
+
+    @Override
+    public int compareTo(Student o){
+        if(this.height == o.height){
+            return o.weight - this.weight;
+        }
+        return this.height - o.height;
+    }
+
+    @Override
+    public String toString(){
+        return height + " " + weight + " " + index;
+    }
+}
