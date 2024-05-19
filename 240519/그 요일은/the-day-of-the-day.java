9a824a09e5124fb3afdf30af7749fffc38b58933
Kang Juhyeong <kakao_panda@naver.com>
2024-05-19 18:52:23 +0900

    Title: 그 요일은, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240519/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    static String[] week = {"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+    static int[] days = {0, 31, 29, 31, 30, 31, 30,
+                       30, 31, 30, 31, 30, 31};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        String w = sc.next();
+
+        int cnt = 0;
+        int monDiff = m2 - m1;
+        int dayDiff = 0;
+
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i=0; i<week.length; i++){
+            map.put(week[i], i);
+        }
+
+        // 05.04 -> 06.03
+        if(d1 > d2){
+            monDiff -= 1;
+            dayDiff += (days[m1] - d1);
+            dayDiff += d2;
+        }
+        else{
+            dayDiff += (d2 - d1);
+        }
+
+        for(int i=1; i<=monDiff; i++){
+            dayDiff += days[m1 + i];
+        }
+        int index = map.get(w);
+        cnt = (dayDiff - index) / 7 + 1;
+
+        System.out.println(cnt);
+    }
+}
