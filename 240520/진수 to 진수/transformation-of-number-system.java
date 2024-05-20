7a68894b7fced41efd42bf1e1a836df02d4c7a2f
Kang Juhyeong <kakao_panda@naver.com>
2024-05-20 23:21:04 +0900

    Title: 진수 to 진수, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240520/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        String n = sc.next();
+
+        System.out.println(Integer.toString(Integer.parseInt(n, a), b));
+    }
+}
