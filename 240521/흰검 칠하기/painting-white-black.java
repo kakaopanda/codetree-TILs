dc6d88bbdd2f645c4595444693d42992c4c5bf59
Kang Juhyeong <kakao_panda@naver.com>
2024-05-21 20:06:21 +0900

    Title: 흰검 칠하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240521/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Color[] arr = new Color[2000];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = new Color();
+        }
+
+        int pos = 1000;
+        for(int i=0; i<n; i++){
+            int move = sc.nextInt();
+            String direction = sc.next();
+
+            if(direction.equals("R")){
+                while(true){
+                    if(move <= 0){
+                        break;
+                    }
+                    else{
+                        arr[pos].blackCnt++;
+                        arr[pos].recentColor = "B";
+                        if(move > 1){
+                            pos++;
+                        }
+                        move--;
+                    }
+                }
+            }
+            else if(direction.equals("L")){
+                while(true){
+                    if(move <= 0){
+                        break;
+                    }
+                    else{
+                        arr[pos].whiteCnt++;
+                        arr[pos].recentColor = "W";
+                        if(move > 1){
+                            pos--;
+                        }
+                        move--;
+                    }
+                }
+            }
+        }
+
+        int whiteCnt = 0;
+        int blackCnt = 0;
+        int grayCnt = 0;
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i].whiteCnt>=2 && arr[i].blackCnt>=2){
+                grayCnt += 1;
+            }
+            else if(arr[i].recentColor.equals("W")){
+                whiteCnt += 1;
+            }
+            else if(arr[i].recentColor.equals("B")){
+                blackCnt += 1;
+            }
+        }
+
+        System.out.println(whiteCnt + " " + blackCnt + " " + grayCnt);
+    }
+}
+
+class Color{
+    int whiteCnt;
+    int blackCnt;
+    String recentColor;
+
+    public Color(){
+        whiteCnt = 0;
+        blackCnt = 0;
+        recentColor = "";
+    }
+}
