e2a4479bc132f470e9e04c6f11109b1a32457b6b
Kang Juhyeong <kakao_panda@naver.com>
2024-05-21 16:08:05 +0900

    Title: 최대로 겹치는 지점, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240521/최대로 겹치는 지점/maximum-overlapped-points.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[101];
+
+        for(int i=0; i<n; i++){
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            
+            for(int j=start; j<=end; j++){
+                arr[j] += 1;
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        for(int i=1; i<arr.length; i++){
+            max = Math.max(max, arr[i]);
+        }
+
+        System.out.println(max);
+    }
+}
