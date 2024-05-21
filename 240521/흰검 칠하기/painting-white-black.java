82cb0161343ddc0c0706336319c5a015192cc47c
Kang Juhyeong <kakao_panda@naver.com>
2024-05-21 20:10:23 +0900

    Title: 흰검 칠하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240521/흰검 칠하기/painting-white-black.java
+++ 240521/흰검 칠하기/painting-white-black.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Color[] arr = new Color[2001];
+        Color[] arr = new Color[200001];
         for(int i=0; i<arr.length; i++){
             arr[i] = new Color();
         }
