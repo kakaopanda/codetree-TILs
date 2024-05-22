2d489283489b2ef3c792b2c3184e221d09f40790
Kang Juhyeong <kakao_panda@naver.com>
2024-05-22 22:06:35 +0900

    Title: 신기한 타일 뒤집기, Time: 222ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240522/신기한 타일 뒤집기/strange-flipping-tiles.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[200000];
+        int pos = 100000;
+
+        for(int i=0; i<n; i++){
+            int move = sc.nextInt();
+            String direction = sc.next();
+
+            if(direction.equals("R")){
+                while(true){
+                    if(move < 0){
+                        break;
+                    }
+                    else{
+                        arr[pos] = 2;
+                        if(move > 1){
+                            pos++;
+                        }
+                        move--;
+                    }
+                }
+            }
+            else if(direction.equals("L")){
+                while(true){
+                    if(move < 0){
+                        break;
+                    }
+                    else{
+                        arr[pos] = 1;
+                        if(move > 1){
+                            pos--;
+                        }
+                        move--;
+                    }
+                }
+            }
+        }
+
+        int whiteCnt = 0;
+        int blackCnt = 0;
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == 1){
+                whiteCnt += 1;
+            }
+            else if(arr[i] == 2){
+                blackCnt += 1;
+            }
+        }
+
+        System.out.println(whiteCnt + " " + blackCnt);
+    }
+}
