277fc5444133235b28f22e5042ad9df9f2e210fd
Kang Juhyeong <kakao_panda@naver.com>
2024-05-23 18:31:16 +0900

    Title: 선두를 지켜라 3, Time: 229ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240523/선두를 지켜라 3/keep-the-lead-3.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] A = new int[1000001];
+        int[] B = new int[1000001];
+
+        int time = 0;
+        for(int i=1; i<=n; i++){
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+            time += t;
+
+            while(t > 0){
+                A[i] = A[i-1] + v;
+                t -= 1;
+            }
+        }
+
+        for(int i=1; i<=m; i++){
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+
+            while(t > 0){
+                B[i] = B[i-1] + v;
+                t -= 1;
+            }
+        }
+
+        int cnt = 0;
+        String rank = "";
+        for(int i=1; i<=time; i++){
+            if(rank.equals("")){
+                if(A[i] > B[i]){
+                    rank = "A";
+                    cnt += 1;
+                }
+                else if(A[i] < B[i]){
+                    rank = "B";
+                    cnt += 1;
+                }
+            }
+            else if(rank.equals("A B")){
+                if(A[i] > B[i]){
+                    rank = "A";
+                    cnt += 1;
+                }
+                else if(A[i] < B[i]){
+                    rank = "B";
+                    cnt += 1;
+                }
+            }
+            else if(rank.equals("A")){
+                if(A[i] < B[i]){
+                    rank = "B";
+                    cnt += 1;
+                }
+                else if(A[i] == B[i]){
+                    rank = "A B";
+                    cnt += 1;
+                }
+            }
+            else if(rank.equals("B")){
+                if(A[i] > B[i]){
+                    rank = "A";
+                    cnt += 1;
+                }
+                else if(A[i] == B[i]){
+                    rank = "A B";
+                    cnt += 1;
+                }
+            }
+        }
+        
+        System.out.println(cnt);
+    }
+}
