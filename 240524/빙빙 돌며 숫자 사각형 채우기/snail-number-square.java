c76bf7b8c69c61c7802d730565e2e99ed4fb9e48
Kang Juhyeong <kakao_panda@naver.com>
2024-05-24 00:07:47 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 234ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240524/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        int[][] map = new int[n][m];
+        
+        // 동(0), 남(1), 서(2), 북(3)
+        int[][] delta = {{0,1},{1,0},{0,-1},{-1,0}};
+        int dir = 0;
+
+        // 동, 남, 서, 북을 반복하며 회전한다. (시계방향 90도 회전)
+        int num = 1;
+        int r = 0;
+        int c = 0;
+        map[r][c] = num++;
+
+        while(num <= n*m){
+            int dr = r + delta[dir][0];
+            int dc = c + delta[dir][1];
+
+            if(isRange(dr, dc) && map[dr][dc]==0){
+                r = dr;
+                c = dc;
+                map[r][c] = num++;
+            }
+            else{
+                dir = (dir + 1) % 4;
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean isRange(int r, int c){
+        return (r>=0 && r<n && c>=0 && c<m);
+    }
+}
