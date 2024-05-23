2d8465786b3beade2690e0f86efbd8dc8578d783
Kang Juhyeong <kakao_panda@naver.com>
2024-05-23 12:32:33 +0900

    Title: 연속되는 수 3, Time: 265ms, Memory: 12MB, Status: Passed - CodeTree

--- 240523/연속되는 수 3/continuous-number3.java
+++ 240523/연속되는 수 3/continuous-number3.java
@@ -11,7 +11,7 @@ public class Main {
         }
 
         int max = 1;
-        int cnt = 0;
+        int cnt = 1;
 
         for(int i=1; i<arr.length; i++){
             if(arr[i-1]>0 && arr[i]>0){
