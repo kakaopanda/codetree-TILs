6104ba277f5f1d699c2e807d0c58b62aec6aa649
Kang Juhyeong <kakao_panda@naver.com>
2024-05-23 17:51:34 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 176ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240523/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int P = sc.nextInt();
+        int T = sc.nextInt();
+
+        Info[] arr = new Info[T];
+        for(int i=0; i<T; i++){
+            // t초에 x개발자와 y개발자가 악수를 나눴다.
+            int t = sc.nextInt();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            arr[i] = new Info(t, x, y);
+        }
+        Arrays.sort(arr);
+
+        Person[] person = new Person[N+1];
+        for(int i=1; i<=N; i++){
+            int val = (i==P) ? 1 : 0;
+            person[i] = new Person(K, val);
+        }
+
+        for(int i=0; i<arr.length; i++){
+            if(person[arr[i].x].infection == 1){
+                if(person[arr[i].x].k > 0){
+                    person[arr[i].x].k -= 1;
+                    person[arr[i].y].infection = 1;
+                }
+            }
+        }
+        
+        for(int i=1; i<person.length; i++){
+            System.out.print(person[i].infection);
+        }
+    }
+}
+
+class Info implements Comparable<Info>{
+    int t;
+    int x;
+    int y;
+
+    public Info(int t, int x, int y){
+        this.t = t;
+        this.x = x;
+        this.y = y;
+    }
+    
+    // 악수를 나눈 시간순서대로 정렬한다.
+    public int compareTo(Info o){
+        return this.t - o.t;
+    }
+}
+
+class Person{
+    int k;
+    int infection;
+
+    public Person(int k, int i){
+        this.k = k;
+        infection = i;
+    }
+}
