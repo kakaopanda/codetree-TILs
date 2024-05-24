ba9c83534791ed565d061b3625b549e4d3a827af
Kang Juhyeong <kakao_panda@naver.com>
2024-05-24 15:55:17 +0900

    Title: 거울에 레이저 쏘기 2, Time: 278ms, Memory: 20MB, Status: Wrong Answer - CodeTree

--- 240524/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
+++ 240524/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
@@ -6,12 +6,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         String[][] map = new String[N][N];
-        
-        // 서쪽(0), 동쪽(1), 남쪽(2), 북쪽(3)
-        int[][] deltaOne = {{0,-1},{0,1},{1,0},{-1,0}};
 
-        // 서쪽(0), 남쪽(1), 북쪽(2), 동쪽(3)
-        int[][] deltaTwo = {{0,-1},{1,0},{-1,0},{0,1}};
+        // 서(0), 남(1), 북(2), 동(3)
+        int[][] delta = {{0,-1},{1,0},{-1,0},{0,1}};
 
         for(int i=0; i<map.length; i++){
             String s = sc.next();
@@ -28,29 +25,17 @@ public class Main {
         dir = changeDirection(dir, map[r][c]);
         
         // 레이저 발사 위치별 반사 유형
-        // (1). "/"
-        // 1). 서쪽 -> 북쪽 2). 북쪽 -> 서쪽 3). 남쪽 -> 동쪽 4). 동쪽 -> 남쪽
-
-        // (2). "\"
-        // 1). 서쪽 -> 남쪽 2). 남쪽 -> 서쪽 3). 동쪽 -> 북쪽 4). 북쪽 -> 동쪽
+        // 1).서<->북 2).동<->남
         int cnt = 0;
         while(isRange(r,c)){
             cnt += 1;
 
-            if(map[r][c].equals("/")){
-                dir = (dir + 2) % 4;
-                int dr = r + deltaOne[dir][0];
-                int dc = c + deltaOne[dir][1];
-                r = dr;
-                c = dc;
-            }
-            else if(map[r][c].equals("\\")){
-                dir = (dir + 2) % 4;
-                int dr = r + deltaTwo[dir][0];
-                int dc = c + deltaTwo[dir][1];
-                r = dr;
-                c = dc;
-            }
+            dir = (dir + 2) % 4;
+            int dr = r + delta[dir][0];
+            int dc = c + delta[dir][1];
+                
+            r = dr;
+            c = dc;
         }
         
         System.out.println(cnt);
@@ -61,36 +46,20 @@ public class Main {
     }
 
     public static int changeDirection(int k, String s){
-        // 남동북서 -> 서남북동
-        if(s.equals("/")){
-            if(k == 0){
-                return 1;
-            }
-            else if(k == 1){
-                return 3;
-            }
-            else if(k == 2){
-                return 2;
-            }
-            else if(k == 3){
-                return 0;
-            }
+        // 남서북동 -> 서남북동
+        if(k == 0){
+            return 1;
         }
-        // 남동북서 -> 서동남북
-        else if(s.equals("\\")){
-            if(k == 0){
-                return 2;
-            }
-            else if(k == 1){
-                return 1;
-            }
-            else if(k == 2){
-                return 3;
-            }
-            else if(k == 3){
-                return 0;
-            }
+        else if(k == 1){
+            return 0;
         }
+        else if(k == 2){
+            return 2;
+        }
+        else if(k == 3){
+            return 3;
+        }
+
         return -1;
     }
 
@@ -98,13 +67,13 @@ public class Main {
         if(k <= N){ // 남쪽 방향(S)
             return 0;
         }
-        else if(k <= 2*N){ // 동쪽 방향(E)
+        else if(k <= 2*N){ // 서쪽 방향(E)
             return 1;
         }
         else if(k <= 3*N){ // 북쪽 방향(N)
             return 2;
         }
-        else if(k <= 4*N){ // 서쪽 방향(S)
+        else if(k <= 4*N){ // 동쪽 방향(S)
             return 3;
         }
         else{
