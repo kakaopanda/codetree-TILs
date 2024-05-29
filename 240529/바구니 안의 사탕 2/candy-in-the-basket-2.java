4012a0ca436058c6583829ed4e71200b42549a9a
Kang Juhyeong <kakao_panda@naver.com>
2024-05-29 18:25:51 +0900

    Title: 바구니 안의 사탕 2, Time: 145ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240529/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[101];
+        
+        for(int i=0; i<n; i++){
+            int candy = sc.nextInt();
+            int index = sc.nextInt();
+            arr[index] += candy;
+        }
+
+        int max = Integer.MIN_VALUE;
+        for(int i=k; i<arr.length-k-1; i++){
+            int sum = 0;
+            for(int j=i-k; j<=i+k; j++){
+                sum += arr[j];
+            }
+            max = Math.max(max, sum);
+        }
+
+        System.out.println(max);
+    }
+}
